f5d753dad95a3dd86c880f651095504651fbcf1f
alexwestcott <alexwestcott24@gmail.com>
2019-12-17 10:54:52 +0000

    [Dan/Alex] Fixes formatting

--- src/components/JourneyCard/JourneyCard.tsx
+++ src/components/JourneyCard/JourneyCard.tsx
@@ -4,7 +4,7 @@ import './JourneyCard.css';
 
 const JourneyCard: React.FC = () => {
   return (
-    <div>
+    <div className="journey-card-div">
       <Card className="journey-card">
         <div className="title-div"><Card.Title>Manchester Piccadilly - Liverpool Lime Street</Card.Title><div className="platform">Platform: 4a</div></div>
         <div>Scheduled departure: 11:12</div>
--- src/components/JourneyCardList/JourneyCardList.tsx
+++ src/components/JourneyCardList/JourneyCardList.tsx
@@ -5,7 +5,7 @@ import './JourneyCardList.css'
 const JourneyCardList: React.FC = () => {
   return (
     <div className="journey-list">
-      <JourneyCard /><JourneyCard /><JourneyCard /><JourneyCard /><JourneyCard /><JourneyCard />
+      <JourneyCard /><JourneyCard /><JourneyCard /><JourneyCard /><JourneyCard /><JourneyCard /><JourneyCard /><JourneyCard /><JourneyCard /><JourneyCard /><JourneyCard /><JourneyCard />
     </div>
   );
 }

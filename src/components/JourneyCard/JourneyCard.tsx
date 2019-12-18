461504dc8060045e02dfc8d4dc316818f4d33307
alexwestcott <alexwestcott24@gmail.com>
2019-12-18 11:45:01 +0000

    [Alex] Journey info retrieved from memory and displayed

--- src/components/JourneyCard/JourneyCard.tsx
+++ src/components/JourneyCard/JourneyCard.tsx
@@ -2,15 +2,27 @@ import React from 'react';
 import {Card} from 'react-bootstrap';
 import './JourneyCard.css';
 
-const JourneyCard: React.FC = () => {
+type JourneyCardProps = {
+  origin?: String,
+  destination?: String,
+  platform?: String,
+  scheduledDepartureTime?: String,
+  estimatedDepartureTime?: String,
+  arrivalTime?: String
+}
+
+const renderLateness = () => {
+    return <span>11:16 (4 mins late)</span>
+}
+
+const JourneyCard: React.FC<JourneyCardProps> = (props) => {
+
   return (
     <div className="journey-card-div">
       <Card className="journey-card">
-        <div className="title-div"><Card.Title>Manchester Piccadilly - Liverpool Lime Street</Card.Title><div className="platform">Platform: 4a</div></div>
-        <div>Scheduled departure: 11:12</div>
-        <div>Estimated departure: {/*only show if train is late*/} 11:16 (4 mins late)</div>
-        <div>Scheduled arrival: 12:05</div>
-
+        <div className="title-div"><Card.Title>{props.origin} - {props.destination}</Card.Title><div className="platform">Platform: {props.platform}</div></div>
+        <div>Departure: <span  style={1 >= 0 ? {textDecorationLine: 'none'} : {textDecorationLine: 'line-through'}}>{props.scheduledDepartureTime}</span>&nbsp;{renderLateness()}</div>
+        <div>Arrival: {props.arrivalTime}</div>
       </Card>
     </div>
   );
--- src/components/JourneyCardList/JourneyCardList.tsx
+++ src/components/JourneyCardList/JourneyCardList.tsx
@@ -1,13 +1,50 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import JourneyCard from '../JourneyCard/JourneyCard';
-import './JourneyCardList.css'
+import './JourneyCardList.css';
+
+const API_URL = "http://localhost:8080/journeys";
 
 const JourneyCardList: React.FC = () => {
+
+  const[journeys, setJourneys] = useState<JourneyType[]>([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  function displayJourneyCards(){
+    return (
+      journeys.map((j, i) => {
+        return <JourneyCard key={i} origin={j.originStation} destination={j.destinationStation} />
+      })
+    );
+  }
+
+  async function fetchData() {
+    const res = await fetch(API_URL).then(result => result.json()).then(result => {
+      for(let i in result["journeys"]){
+        setJourneys(journeys => [...journeys, new JourneyType(result["journeys"][i]["originStation"]["name"], result["journeys"][i]["destinationStation"]["name"])]);
+      }
+
+    });
+  }
+
   return (
     <div className="journey-list">
-      <JourneyCard /><JourneyCard /><JourneyCard /><JourneyCard /><JourneyCard /><JourneyCard /><JourneyCard /><JourneyCard /><JourneyCard /><JourneyCard /><JourneyCard /><JourneyCard />
+      {displayJourneyCards()}
     </div>
   );
+}
+
+class JourneyType {
+  constructor(originStation: string, destinationStation: string){
+    this.originStation = originStation;
+    this.destinationStation = destinationStation;
+    this.key = Math.random() * 10000000;
+  }
+  originStation: String;
+  destinationStation: String;
+  key: number;
 }
 
 export default JourneyCardList;

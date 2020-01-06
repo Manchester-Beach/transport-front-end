16ea78e550f38092d0e9978bcade1a1295aa26ed
alexwestcott <alexwestcott24@gmail.com>
2020-01-06 14:23:32 +0000

    minor fixes

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,51 +1,27 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import Header from "../Header/Header";
 import JourneyCardList from "../JourneyCardList/JourneyCardList";
 import JourneyForm from "../JourneyForm/JourneyForm";
 import "./App.css";
-import { JourneyType } from "../../utils/Types";
 
-const API_URL = "http://localhost:8080/journeys";
 
 const App: React.FC = () => {
 
-  const [journeys, setJourneys] = useState<JourneyType[]>([]);
-
-  async function refreshCards(){
-    setJourneys([]);
-    fetchData();
+  function callbackFunction(data: any){
+    console.log("do something!!");
+    return data;
   }
-
-  function addJourney(data: JourneyType){
-      setJourneys(journeys=>[...journeys, data]);
-  }
-
-  useEffect(() => {
-    fetchData();
-  }, []);
-
   return (
     <div className="App">
       <header className="App-header">
         <Header />
       </header>
       <div id="main">
-        <JourneyForm refreshPage={refreshCards}/>
-        <JourneyCardList refreshCards={refreshCards} journeys={journeys} addJourneys={addJourney}/>
+        <JourneyForm refreshPage={callbackFunction}/>
+        <JourneyCardList />
       </div>
     </div>
   );
-
-  async function fetchData() {
-    await fetch(API_URL).then(result => result.json()).then(result => {
-      for(let i in result["journeys"]){
-        addJourney(new JourneyType(result["journeys"][i]["originStation"]["crs"],
-        result["journeys"][i]["destinationStation"]["crs"],
-        result["journeys"][i]["originStation"]["name"],
-        result["journeys"][i]["destinationStation"]["name"]));
-      }
-    });
-  }
 };
 
 export default App;

1f9cafbd67de05a6c0616ab7d8263c2fda0ec824
alexwestcott <alexwestcott24@gmail.com>
2020-01-06 14:21:41 +0000

    [Alex] Cancelled journeys now show next possible train

--- src/components/JourneyCard/JourneyCard.tsx
+++ src/components/JourneyCard/JourneyCard.tsx
@@ -33,6 +33,7 @@ const JourneyCard: React.FC<JourneyCardProps> = (props) => {
   const [scheduledDeparture, setScheduledDeparture] = useState("");
   const [estimatedDeparture, setEstimatedDeparture] = useState("");
   const [arrivalTime, setArrivalTime] = useState("");
+  const [nextTrain, setNextTrain] = useState("");
 
   useEffect(() => {
     fetchData();
@@ -42,7 +43,7 @@ const JourneyCard: React.FC<JourneyCardProps> = (props) => {
 
   function fetchData(){
     console.log("Refreshing journey info at " + new Date().toLocaleTimeString());
-    fetch(API_SCHED_URL + props.originCrs + "/" + props.destinationCrs).then(
+    fetch(API_SCHED_URL + props.originCrs + "/" + props.destinationCrs + "/0").then(
       response => {
         const data = response.json();
         return JSON.stringify(data) == null ? null : data;
@@ -50,32 +51,42 @@ const JourneyCard: React.FC<JourneyCardProps> = (props) => {
       }
     ).then(data =>
     {
+      if(data["cancelled"]){
+        fetchFurtherJourneys(1);
+      }
       setPlatform(data["platform"]);
       setScheduledDeparture(data["scheduledDeparture"]);
       setEstimatedDeparture(data["expectedDeparture"]);
       setArrivalTime(data["arrivalTime"]);
       setCancelled(data["cancelled"]);
-      }
+    }
     );
+  }
+
+  function fetchFurtherJourneys(journeyIndex: number){
+    fetch(API_SCHED_URL + props.originCrs + "/" + props.destinationCrs + "/" + journeyIndex).then(
+      response => {
+        const data = response.json();
+        return JSON.stringify(data) == null ? null : data;
+      }
+    ).then(data =>
+      {
+        if(data["cancelled"]){
+          fetchFurtherJourneys(journeyIndex + 1);
+        }
+
+        setNextTrain("Next train is at " + data["scheduledJourneys"] + " from platform " + data["platform"]);
+      }
+    )
   }
 
   const journeyLateClassNames = scheduledDeparture === estimatedDeparture && estimatedDeparture !== undefined ? "journey-card journey-on-time" : "journey-card journey-late";
   return (
     <div className="journey-card-div">
       <Card className={journeyLateClassNames}>
-        <div className="title-div">
-          <Card.Title>{props.origin} - {props.destination}</Card.Title>
-          {cancelled || scheduledDeparture === undefined ? null : <div className="platform">Platform: {platform}</div>}
-        </div>
-        <div className="middle-row">
-          {scheduledDeparture !== undefined ? 
-          <div>Departure: {showDepartureTime(scheduledDeparture, estimatedDeparture, cancelled)}&nbsp;
-          </div> : <div>No direct train available!</div>}
-          <div onClick={props.parentCallback}>
-            <IconButton aria-label="delete" className="delete-button" size="small"><DeleteIcon fontSize="small" /></IconButton>
-            </div>
-          </div>
-        {scheduledDeparture !== undefined ? (cancelled ? <span>&nbsp;</span> : <div>Arrival: {arrivalTime}</div>) : null }
+        <div className="title-div"><Card.Title>{props.origin} - {props.destination}</Card.Title>{cancelled || scheduledDeparture === undefined ? null : <div className="platform">Platform: {platform}</div>}</div>
+        <div className="middle-row">{scheduledDeparture !== undefined ? <div>Departure: {showDepartureTime(scheduledDeparture, estimatedDeparture, cancelled)}&nbsp;</div> : <div>No direct train available!</div>}<div onClick={props.parentCallback}><IconButton aria-label="delete" className="delete-button" size="small"><DeleteIcon fontSize="small" /></IconButton></div></div>
+        {scheduledDeparture !== undefined ? (cancelled ? <span>{nextTrain}</span> : <div>Arrival: {arrivalTime}</div>) : null }
       </Card>
     </div>
   );

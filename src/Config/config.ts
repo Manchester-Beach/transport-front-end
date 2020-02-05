a3b4431e6f7411a75e8b74018e67a5a388e88c0d
Matthew Peers <peersmg@gmail.com>
2020-02-05 14:19:47 +0000

    [Matt][#09] Alters config import

--- src/Config/config.ts
+++ src/Config/config.ts
@@ -1,5 +1,7 @@
-export class Config {
+class Config {
     static useProductionApi = true;
     static productionApiUrl = "https://beach-train-backend.herokuapp.com/";
     static localApiURL = "http://localhost:8080/";
 };
+
+export default Config;
--- src/utils/ApiService.tsx
+++ src/utils/ApiService.tsx
@@ -1,5 +1,5 @@
 import { Station } from "./Types";
-import { Config } from "../config/config";
+import Config from "../config/config";
 
 class ApiService {
   baseUrl: string;
